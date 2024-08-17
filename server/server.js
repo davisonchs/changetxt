6c2ebe41763c5e2125ed09f45a556a792025b896
Davison Hernandez <davisonchs@live.com>
2024-08-16 23:46:51 -0500

    path usage

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Correct path usage
 const distPath = path.join(__dirname, '../client/dist');
 console.log('Serving static files from:', distPath);
 
 app.use(express.static(distPath));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

154cc00bc12fd8d00c9cc43ee8aa723040d9bea2
Davison Hernandez <davisonchs@live.com>
2024-08-16 22:30:12 -0500

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+const distPath = path.join(__dirname, '../client/dist');
+console.log('Serving static files from:', distPath);
+
+app.use(express.static(distPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
